571028be3f40a7d66c7ab8a6a5c81e6376810c8f
Ahmedok <a_sarinov@mail.ru>
2026-06-16 07:20:24 +0000

    feat: Add protected routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,27 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
+import CalculatorPage from "./pages/CalculatorPage";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/calculator"
+            element={
+              <ProtectedRoute>
+                <CalculatorPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
